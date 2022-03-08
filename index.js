3b8a40c9312e79665a4eb798e92d8e441074006b
nehoraiharush <101175556+nehoraiharush@users.noreply.github.com>
2022-03-08 12:19:04 +0200

    8/3/2022

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello from ROUTES");
+const express = require('express');
+//import express from 'express';//same as line 1
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+const port = 5090;
+app.listen(port, function () {
+    console.log(`Server is running via port ${port}`);
+});
+
